665e2d820c658eebdb187ec01a87aadc1df316af
Raphael Katana <roduor41@gmail.com>
2018-10-29 09:32:33 +0300

    edit user class

---
+++ src/app/user.ts
@@ -0,0 +1,17 @@
+import { environment } from "src/environments/environment";
+
+export class User {
+  
+  constructor(
+    public User:any,
+    public login:string,
+    public html_url:string,
+    public followers:number,
+    public following:number,
+    public public_repos:number
+    ){
+    // this.User='https://api.github.com/users/RKatana?access_token' +environment.apiKey
+
+    this.html_url='https://github.com/RKatana'
+  }
+}
